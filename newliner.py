307c5a502d0bcb59c0908ca9640da6fe2e4032a0
syhsblfthr <abil.f.nasution@gmail.com>
2023-11-25 16:35:59 +0700

    v1.3 file and stdin
    [+] added support for stdin input aside from filepath input
    [-] removed tests for now

--- newliner.py
+++ newliner.py
@@ -2,19 +2,46 @@ import sys
 import io
 import os
 
-argv = sys.argv
-argc = len(argv)
-if (argc <= 1):
+def print_usage():
     script_full_name    = __file__
     rmost_delimiter     = script_full_name.rfind(os.sep)
     script_name         = script_full_name[((rmost_delimiter + 1) if rmost_delimiter else 0) : None]
     sys.exit(str.format("Usage\n\tpy {0} INPUT_FILE [OUTPUT_FILE]", script_name))
 
-inputfile   = argv[1]
-file        = open(inputfile, 'r')
-string      = file.read()
-file.close()
+if __name__ == '__main__':
+    argv            = sys.argv
+    argc            = len(argv)
+    string          = None
+    string_input    = None
+    file_handle     = None
 
-file = open(argv[2] if argc >= 3 else inputfile, 'w')
-file.write(string.replace(';', '\n'))
-file.close()
+    if argc <= 1:
+        print_usage()
+
+    flag = argv[1]
+    if not flag.startswith('--'):
+        flag = None
+
+    input_file  = argv[2 if flag else 1]
+
+    output_file = None
+    if not flag and argc >= 3:
+        output_file = argv[2]
+    elif flag and argc >= 4:
+        output_file = argv[3]
+
+    if flag and flag.lstrip('--').lower() == 'f':
+        file_handle = open(input_file, 'r')
+        string      = file_handle.read().replace(';', '\n')
+
+        file_handle.close()
+    else:
+        string = input_file.replace(';', '\n')
+
+    if output_file:
+        file_handle = open(output_file, 'w')
+
+        file_handle.write(string)
+        file_handle.close()
+    else:
+        print(string)
