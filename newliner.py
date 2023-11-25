1a6c0814f01eb93f9023668406d1be2f53bb8502
syhsblfthr <abil.f.nasution@gmail.com>
2023-11-25 10:03:17 +0700

    make the script name dynamic instead of static/hard-coded

--- newliner.py
+++ newliner.py
@@ -1,10 +1,14 @@
 import sys
 import io
+import os
 
 argv = sys.argv
 argc = len(argv)
 if (argc <= 1):
-    sys.exit(str.format("Usage\n\tpy {0} INPUT_FILE [OUTPUT_FILE]", __file__))
+    script_full_name    = __file__
+    rmost_delimiter     = script_full_name.rfind(os.sep)
+    script_name         = script_full_name[((rmost_delimiter + 1) if rmost_delimiter else 0) : None]
+    sys.exit(str.format("Usage\n\tpy {0} INPUT_FILE [OUTPUT_FILE]", script_name))
 
 inputfile   = argv[1]
 file        = open(inputfile, 'r')
